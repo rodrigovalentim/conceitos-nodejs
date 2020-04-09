0ac8214fdb1efeca6c7c555183f9c3ac8033969f
Rodrigo Valentim <rodrigo.valentim@gmail.com>
2020-04-09 17:17:23 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid} = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,75 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+  
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid Repository Id'});
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id );
+  
+  if (index < 0 ) {
+    return response.status(400).json({ error: 'Invalid Repository ID'});
+  }
+  const { title, url, techs } = request.body;
+  const { likes } = repositories[index];
+  //let repository = repositories[index]
+  
+  const repository = {id, title, url, techs, likes };
+  console.log(repository);
+  repositories[index] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id );
+  
+  if (index < 0 ) {
+    return response.status(400).json({ error: 'Invalid Repository ID'});
+  }
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id );
+  
+  if (index < 0 ) {
+    return response.status(400).json({ error: 'Invalid Repository ID'});
+  }
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
